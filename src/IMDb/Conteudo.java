aa480e53944e93225441430c13860f41fe80f06f
bruno7ferreira <b.rogerio2007@gmail.com>
2023-04-01 13:38:02 -0400

    Atualizando a classe conteúdo.

--- src/IMDb/Conteudo.java
+++ src/IMDb/Conteudo.java
@@ -2,7 +2,23 @@ package IMDb;
 
 public class Conteudo {
 
-    String titulo;
-    String urlImagem;
+    //atributos
+    private String titulo;
+    private String urlImagem;
+
+    //Construtor
+    public Conteudo(String titulo, String urlImagem) {
+        this.titulo = titulo;
+        this.urlImagem = urlImagem;
+    }
+
+    //get
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
 
 }
