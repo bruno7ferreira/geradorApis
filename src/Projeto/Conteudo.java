d448f1db146e1a462e47917ddb976f172042f0bd
bruno7ferreira <b.rogerio2007@gmail.com>
2023-04-02 19:57:57 -0400

    Atualizando o projeto

--- src/Projeto/Conteudo.java
+++ src/Projeto/Conteudo.java
@@ -1,25 +1,11 @@
 package Projeto;
 
-public class Conteudo {
-
-    //atributos
-    private final String titulo;
-    private final String urlImagem;
-
-    //Construtor
-    public Conteudo(String titulo, String urlImagem) {
-        this.titulo = titulo;
-        this.urlImagem = urlImagem;
-    }
-
-    //get
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getUrlImagem() {
-        return urlImagem;
-    }
-
+public record Conteudo(String titulo, String urlImagem) {
 
 }
+
+
+
+
+
+
